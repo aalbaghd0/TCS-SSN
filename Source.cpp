8fcefffa67a5c192b2d930792200ab19f2ed9775
aalbaghd0 <aalbaghd0@gmail.com>
2019-11-07 10:45:26 -0500

    edit

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vertex.h>
+#define const int CKINS = 6;
+
 
 void main() {
 	std::cout << "Hello";
--- vertex.h
+++ vertex.h
@@ -1,11 +1,11 @@
 #pragma once
 #ifndef VERTEX_H
 #define VERTEX_H
-
+const int CKINS = 6 * 2; // 6 ckin locations
 class vertex {
 public:
 	int id;
-	int 
+	double ckins[CKINS];
 
 };
 
